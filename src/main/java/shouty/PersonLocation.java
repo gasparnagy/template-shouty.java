52d420599221d4d9c6b3dec1348f6e9f0c5305ed
Gaspar Nagy <gaspar.nagy@gmail.com>
2020-04-06 12:01:07 +0200

    P10-data-tables: Introduce PersonLocation and use it in step definition

---
+++ src/main/java/shouty/PersonLocation.java
@@ -0,0 +1,13 @@
+package shouty;
+
+public class PersonLocation {
+    public String name;
+    public int x;
+    public int y;
+
+    public PersonLocation(String name, int x, int y) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+}
