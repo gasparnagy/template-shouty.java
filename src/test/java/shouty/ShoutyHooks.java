d59a9ec01d7ef14189dbd1d7fb78857a3f117c08
Gaspar Nagy <gaspar.nagy@gmail.com>
2020-04-06 12:20:06 +0200

    P20-hooks: Add ordered hooks

--- src/test/java/shouty/ShoutyHooks.java
+++ src/test/java/shouty/ShoutyHooks.java
@@ -5,13 +5,23 @@ import io.cucumber.java.After;
 
 public class ShoutyHooks {
 
-    @Before
+    @Before(order = 1)
     public void beforeScenario(){
         System.out.println("this is before");
     }
 
-    @After
+    @After(order = 1)
     public void afterScenario(){
         System.out.println("this is after");
+    }
+
+    @Before(order = 2)
+    public void otherBeforeScenario(){
+        System.out.println("this is other before");
+    }
+
+    @After(order = 2)
+    public void otherAfterScenario(){
+        System.out.println("this is other after");
     }
 }
