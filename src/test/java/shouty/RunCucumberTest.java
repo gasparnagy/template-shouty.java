5449d8e1eae09693e13519e45c61d693ef5c1907
Gaspar Nagy <gaspar.nagy@gmail.com>
2020-04-06 09:54:59 +0200

    Project init

---
+++ src/test/java/shouty/RunCucumberTest.java
@@ -0,0 +1,22 @@
+package shouty;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/cucumber.html",
+                "rerun:target/rerun.txt",
+                "json:target/cucumber.json"
+        },
+        snippets = SnippetType.CAMELCASE
+)
+public class RunCucumberTest {
+  // this is the adapter/bridge code
+  // between cucumber jvm and junit
+  // you do not need to edit this
+}
